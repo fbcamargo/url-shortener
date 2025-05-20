8cb685882ff34120b6a1b9f023b283a462dd87c5
Felipe <camargo963@gmail.com>
2025-05-20 07:36:48 -0300

    Removendo lógica do repository

--- src/repositories/MongooseUrlRepository.js
+++ src/repositories/MongooseUrlRepository.js
@@ -1,4 +1,3 @@
-const { nanoid } = require("nanoid");
 const Url = require("../models/Url.js");
 
 class MongooseUrlRepository {
@@ -8,29 +7,12 @@ class MongooseUrlRepository {
         return url;
     }
 
-    async createOrRenew(createUrlDto) {
-        const existing = await Url.findOne({ originalUrl: createUrlDto.url }).sort({ _id: -1 });
-        const isExpired = existing && existing.expiresAt <= Date.now();
-
-        if (!existing || isExpired) {
-            return Url.create({
-                originalUrl: createUrlDto.url,
-                shortId: this.#generateShortId(5, 10),
-                expiresAt: this.#getExpirationDate(),
-                clicks: 0
-            });
-        }
-        return existing;
+    async findByOriginalUrl(url) {
+        return Url.findOne({ originalUrl: url }).sort({ _id: -1 });
     }
 
-    #generateShortId(min, max) {
-        const size = Math.random() * (max - min) + min;
-        return nanoid(size);
-    }
-
-    #getExpirationDate() {
-        const now = new Date();
-        return now.setHours(now.getHours() + 24);
+    async create(data) {
+        return Url.create(data);
     }
 }
 
--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -1,3 +1,5 @@
+const { nanoid } = require("nanoid");
+
 class UrlService {
     constructor(urlRepository) {
         this.urlRepository = urlRepository;
@@ -8,9 +10,29 @@ class UrlService {
     }
 
     async createOrRenew(createUrlDto) {
-        return this.urlRepository.createOrRenew(createUrlDto);
+        const existing = await this.urlRepository.findByOriginalUrl(createUrlDto.url);
+        const isExpired = existing && existing.expiresAt <= Date.now();
+
+        if (!existing || isExpired) {
+            return this.urlRepository.create({
+                originalUrl: createUrlDto.url,
+                shortId: this.#generateShortId(5, 10),
+                expiresAt: this.#getExpirationDate(),
+                clicks: 0
+            });
+        }
+        return existing;
+    }
+
+    #generateShortId(min, max) {
+        const size = Math.random() * (max - min) + min;
+        return nanoid(size);
+    }
+
+    #getExpirationDate() {
+        const now = new Date();
+        return now.setHours(now.getHours() + 24);
     }
 }
 
 module.exports = UrlService;
-
