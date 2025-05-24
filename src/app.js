12fe14fbd3cdcc611f249e052f16a7cea6487635
Felipe <camargo963@gmail.com>
2025-05-24 00:08:00 -0300

    fix app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/index");
 
-app = express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
