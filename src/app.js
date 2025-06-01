79a19aa822c208947f276abcd530d322e5c4be19
Felipe <camargo963@gmail.com>
2025-06-01 11:57:01 -0300

    Add Helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/index");
+const helmet = require("helmet");
 
 const app = express();
 app.use(cors());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'"]
+        }
+    },
+    frameguard: { action: 'deny'},
+    hidePoweredBy: true,
+    noSniff: true,
+    referrerPolicy: { policy: 'no-referrer' }
+}));
 app.use(express.json());
 app.use("/", routes);
 
